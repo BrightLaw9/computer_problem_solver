e4b37af401ce2097de4f22b77f16704b74d1c440
Gary Xiong <xionghjg1723@gmail.com>
2022-12-10 17:33:32 -0500

    costume = amongus

--- web_scraper_algorithm.py
+++ web_scraper_algorithm.py
@@ -1,11 +1,6 @@
 from bs4 import BeautifulSoup
 import requests 
-import os 
-from collections import defaultdict 
-
-# Unlimited arguments can be passed into this function, following this format (keyword=category_name)
-# Note: Category_name will be the folder which the images will be inserted into 
-
+import random
 
 def print_hello_world():
     print('hello world')
@@ -19,22 +14,49 @@ def remove_url_prefix(url):
         url = url[7:] 
     return url
 
-def find_websites_from_keyword(unique_websites, *args): 
+def find_websites_from_keyword(keyword): 
     #Args as a string
-    for word in args:
-        url = 'https://www.google.com/search?q=' + word + '&source=lnms&tbm=isch'
-        page = requests.get(url) 
-        soup = BeautifulSoup(page.content, 'html.parser') 
-        for i in soup.find_all('a'): 
-            link = i['href']
-            mainPart = link.split("&")[0]
-            mainPart = remove_url_prefix(mainPart)
-            add_to_unique_list(mainPart, unique_websites)
+    unique_websites = []
+   
+    url = 'https://www.google.com/search?q=' + keyword + '&source=lnms&tbm=isch'
+    page = requests.get(url) 
+    soup = BeautifulSoup(page.content, 'html.parser') 
+    for i in soup.find_all('a'): 
+        link = i['href']
+        mainPart = link.split("&")[0]
+        mainPart = remove_url_prefix(mainPart)
+        add_to_unique_list(mainPart, unique_websites)
+    return unique_websites
         
-def filter_by_domain(domain, unique_websites): 
+def filter_by_domain(unique_websites): 
     domain_websites = []
     for website in unique_websites: 
-        if website.startswith(f"https://{domain}"): 
+        if ".org" in website or ".edu" in website or "w3schools" in website \
+        or "codecademy" in website or "stackoverflow" in website and "wiki" not in website: 
             domain_websites.append(website)
-    
     return domain_websites
+
+def filter_quantity_of_websites(quantity, unique_websites): 
+    websites_in_quantity = []
+    selected_idxes = []
+
+    if len(unique_websites) == 0: 
+        return ["No websites found"]
+
+    if len(unique_websites) <= quantity: 
+        return unique_websites 
+    
+    for i in range(quantity): 
+        selected_idx = random.randint(0, len(unique_websites)-1)
+        selected_idxes.append(selected_idx)
+        while selected_idx in selected_idxes: 
+            # Reselect index if all ready selected before, until it is unique
+           selected_idx = random.randint(0, len(unique_websites)-1) 
+
+        #Get the website corresponding to the index 
+        website = unique_websites[selected_idx]
+        websites_in_quantity.append(website)
+    
+    return websites_in_quantity
+    
+        
