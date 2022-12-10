d580748bc2cf94dd3171a1eaa1229894916ff3a2
Gary Xiong <xionghjg1723@gmail.com>
2022-12-10 13:43:43 -0500

    imported algorithms

--- README.md
+++
@@ -1,1 +0,0 @@
-# computer_problem_solver
---
+++ web_scraper_algorithm.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import requests 
+import os 
+from collections import defaultdict 
+
+# Unlimited arguments can be passed into this function, following this format (keyword=category_name)
+# Note: Category_name will be the folder which the images will be inserted into 
+
+
+def print_hello_world():
+    print('hello world')
+
+def add_to_unique_list(website): 
+    if website not in unique_websites: 
+        unique_websites.append(website)
+
+def remove_url_prefix(url):  
+    if url.startswith("/url?q="): 
+        url = url[7:] 
+    return url
+
+def find_websites_from_keyword(word): 
+    #Args as a string
+    url = 'https://www.google.com/search?q=' + word + '&source=lnms&tbm=isch'
+    page = requests.get(url) 
+    soup = BeautifulSoup(page.content, 'html.parser') 
+    for i in soup.find_all('a'): 
+        link = i['href']
+        mainPart = link.split("&")[0]
+        mainPart = remove_url_prefix(mainPart)
+        add_to_unique_list(mainPart)
+    
+def filter_by_domain(domain): 
+    domain_websites = []
+    for website in unique_websites: 
+        if website.startswith(f"https://{domain}"): 
+            domain_websites.append(website)
+    
+    return domain_websites
