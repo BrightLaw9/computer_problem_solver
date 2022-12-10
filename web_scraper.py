beb18c19cb91b8089bf99eaf1cf334208b5d35ec
Gary Xiong <xionghjg1723@gmail.com>
2022-12-10 13:55:10 -0500

    no delete

--- web_scraper.py
+++ web_scraper.py
@@ -2,15 +2,20 @@
 from tkinter import *
 from web_scraper_algorithm import *
 from bs4 import BeautifulSoup
-import requests 
-import os 
-from collections import defaultdict 
+#import requests 
+
+menu = 0
 
 unique_websites = []
+def generate_sites():
+    user_keyword = 
+    find_websites_from_keyword(unique_websites, user_keyword)
 
-#find_websites_from_keyword(unique_websites, "python_keywords")
+def call_filter():
+    user_site = site.get() 
+    filter_by_domain(user_site, unique_websites)
 
-print(unique_websites)
+def call_search():
 
 root = Tk()
 mainframe = Frame(root)
@@ -22,19 +27,29 @@ creatorsLabel = Label(mainframe, text = 'Gary, Rayan, Lawrence, Rohan', font = p
 v = Scrollbar(root, orient='vertical')
 #v.config(command=t.yview)
 
-user_choice = Label(mainframe, text= "How much ")
-
+user_choice = Label(mainframe, text= "How many websites would you like us to search?", font=pixel_font_small)
+start_search = Button(mainframe, text='START', command = call_search)
 
 user_amount = IntVar()
 site_numbers = Scale(mainframe, from_=1, to=10, orient=HORIZONTAL, variable=user_amount, font=pixel_font_small)
 
+site_question = Label(mainframe, text="Which site would you want to look at?")
 
+site = StringVar()
+site_entry = Entry(mainframe, textvariable=site)
+
+generate_list_button = Button(mainframe, text="Generate Sites", command = generate_sites)
 
 #Gridding the widgets
 root.minsize(450, 400)
 mainframe.grid(padx = 50, pady = 50)
-titleLabel.grid(row=2, column=1, columnspan=2)
-creatorsLabel.grid(row=2, column=1, columnspan=2)
+
+if menu == 0:
+    titleLabel.grid(row=1, column=1, columnspan=2)
+    creatorsLabel.grid(row=2, column=1, columnspan=2)
+
+if menu == 1:
+user_choice.grid(row=3, column=1, columnspan=2)
 site_numbers.grid(row=4, column=1, columnspan=2)
 v.grid(row=1, column=1, rowspan = 3)
 
